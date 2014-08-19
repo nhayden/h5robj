0d43792e79e3ad6069d18781dca200c74e624d17
nhayden <nhayden@fhcrc.org>
2014-08-19 09:28:07 -0700

    Recurisve encoding of S4 and some primitives, but without: - encoding/tracking NULL slots, - class / SXP type attributes on H5 objects

--- R/h5encode.R
+++ R/h5encode.R
@@ -14,13 +14,13 @@ setGeneric("encode")
 setMethod("encode", "ANY", function(obj, file, name, ...) {
     if (isS4(obj)) {
         message("S4!")
-        browser()
+        ##browser()
         h5createGroup(file, name)
         ## recursively encode each attribute
         encode_attributes(attributes(obj), file, name, ...)
     } else {
         message("not S4!")
-        browser()
+        ##browser()
         h5createGroup(file, name)
         if ("class" %in% names(attributes(obj))) {
             stop("S3 not implemented")
@@ -39,7 +39,7 @@ encode_attributes <- function(attrs, file, name, ...) {
     if(is.null(attrs))
         return()
     message("encode_attributes!")
-    browser()
+    ##browser()
     if( !is.list(attrs) )
         stop("attrs must be list, got ", class(attrs))
     if( !all(names(attrs) != ""))
@@ -57,34 +57,19 @@ encode_attributes <- function(attrs, file, name, ...) {
 
 encode.name <- function(obj, file, name, ...) {
     message("encode.name!")
-    browser()
+    ##browser()
     encode_attributes(attributes(obj), file, name, ...)
-    attributes(obj) <- NULL
-    class(obj) <- "name"
-    h5write(as.character(obj), file, paste(name, "name", sep="/"),
+    h5write(as.character(obj), file, paste(name, "data", sep="/"),
             write.attributes=TRUE)
 }
 
 encode.default <- function(obj, file, name, ...) {
     message("encode.default!")
-    browser()
+    ##browser()
     data_name <- paste(name, "data", sep="/")
     encode_attributes(attributes(obj), file, name, ...)
     h5write(obj, file, data_name, ...)
 }
-
-## setMethod("encode", "integer", function(obj, file, name, ...) {
-##     encode_primitive(obj, file, name, ...)
-## })
-## setMethod("encode", "double", function(obj, file, name, ...) {
-##     encode_primitive(obj, file, name, ...)
-## })
-## setMethod("encode", "logical", function(obj, file, name, ...) {
-##     encode_primitive(obj, file, name, ...)
-## })
-## setMethod("encode", "character", function(obj, file, name, ...) {
-##     encode_primitive(obj, file, name, ...)
-## })
 
 ## setMethod("encode", "matrix", function(obj, ...) {
     
--- inst/unitTests/test_h5encode.R
+++ inst/unitTests/test_h5encode.R
@@ -22,7 +22,41 @@ test_write_IRanges <- function() {
     if(interactive())
         message(h5fl)
     h5createFile(h5fl)
+    top_name <- "foo"
     
-    rhdf5:::encode(ir, h5fl, "foo")
+    rhdf5:::encode(ir, h5fl, top_name)
+
+    ## test start
+    path <- paste(top_name, "start", "data", sep="/")
+    start <- as.integer(h5read(h5fl, path))
+    checkIdentical(start(ir), start)
+    
+    ## test width
+    path <- paste(top_name, "width", "data", sep="/")
+    width <- as.integer(h5read(h5fl, path))
+    checkIdentical(width(ir), width)
+
+    ## test NAMES
+    ## XXXX FIX ME: what to do??
+    path <- paste(top_name, "NAMES", "data", sep="/")
+    NAMES <- as.name(h5read(h5fl, path))
+    checkIdentical(as.name('\001NULL\001'), NAMES)
+
+    ## test elementMetadata / mcols
+    ## XXXX FIX ME: what to do?
+    path <- paste(top_name, "elementMetadata", "data", sep="/")
+    elementMetadata <- as.name(h5read(h5fl, path))
+    checkIdentical(as.name('\001NULL\001'), elementType)
+
+    ## test elementType
+    path <- paste(top_name, "elementType", "data", sep="/")
+    elementType <- as.character(h5read(h5fl, path))
+    checkIdentical(elementType(ir), elementType)
+
+    ## test metadata?
+    ## XXXX FIX ME: check for absence of dataset called 'metadata'?
+
+    ## test
+    
 }
 test_write_IRanges()
