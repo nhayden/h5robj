5bbf407808d06f8490c2192ae2f0b2cb6f8b23e3
nhayden <nhayden@fhcrc.org>
2014-11-25 13:11:42 -0800

    messing around with selection policy functions

--- TICKLER.R
+++ TICKLER.R
@@ -18,3 +18,35 @@ sel = Selector(h5fl, "foo")
 ## Example of contains
 track <- setClass("track", slots=c(x="numeric", y="numeric"))
 trackCurve <- setClass("trackCurve", slots=c(smooth="numeric"), contains="track")
+
+## This is the inverse!!
+.A <- setClass("A", slots=c(x="numeric"))
+.B <- setClass("B", slots=c(y="numeric", b="A", bget="function"))
+B <- function(y, b) { .B(y=y, b=b, bget=function(thing) thing@x)}
+Bget <- function(whoa) { whoa@bget(whoa@b) }
+anA <- .A(x=15)
+aB <- B(13, anA)
+Bget(aB)
+aB@b@x <- 16
+Bget(aB)
+
+.A <- setClass("A", slots=c(x="numeric", y="function"))
+A <- function(x=42, y=function(fancy) function(i) slot(fancy, i)) .A(x=x, y=y)
+.B <- setClass("B", slots=c(z="numeric", foo="A"))
+B <- function(z, foo=A()) {
+    theB <- .B(z=z, foo=foo)
+    theB
+}
+doit <- function(aB, slotname) {
+    aB@foo@y(aB)(slotname)
+}
+theB <- B(13, A())
+doit(theB, "z")
+theB@z = 26
+extractA <- function(aB) {
+    inner <- aB@foo
+    inner@y <- inner@y(aB)
+    inner
+}
+extracted <- extractA(theB)
+extracted@y("z")
